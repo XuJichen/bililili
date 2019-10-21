0ff2e49b12c3749058506ab4ff48fc3d4ff27c6a
e <2597147885@qq.com>
2019-10-21 16:46:25 +0800

    第二次提交

--- script/aui-tab.js
+++ script/aui-tab.js
@@ -33,15 +33,14 @@
                         if(!self.params.repeatClick){
                             if(this.className.indexOf("aui-active") > -1)return;
                         }
-                        if(this.parentNode.querySelector(".aui-active"))this.parentNode.querySelector(".aui-active").classList.remove("aui-active");
-                        this.classList.add("aui-active");
         				if(callback){
                             callback({
                                 index: parseInt(this.getAttribute("data-item-order"))+1,
                                 dom:this
                             })
                         };
-
+        				this.parentNode.querySelector(".aui-active").classList.remove("aui-active");
+            			this.classList.add("aui-active");
         			}
         		}
         	}
